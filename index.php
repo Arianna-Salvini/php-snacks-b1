0b0366e3b48451399023f59a636f5c719e709160
“Arianna-Salvini” <arianna.salvini@gmail.com>
2024-04-15 12:37:19 +0200

    Add other team

--- index.php
+++ index.php
@@ -1,20 +1,32 @@
 <?php
 
-matches = [
-    'home_team':'Milano',
-    'host_team':'Cantù',
-    'home_score':'55',
-    'host_score':'60',
+$matches = [
+    [
+        'home_team':'Milano',
+        'guest_team':'Cantù',
+        'home_score':'55',
+        'guest_score':'60',
+    ];
+
+    [
+        'home_team':'Verona',
+        'guest_team':'Bologna',
+        'home_score':'53',
+        'guest_score':'66',
+    ];
+
+    [
+        'home_team':'Bolzano',
+        'guest_team':'Trento',
+        'home_score':'23',
+        'guest_score':'67',
+    ];
+        
 ]
-
-for ($i=0; $i < counr($matches); $i++) { 
-   $match = $matches[$i]
-
-}
 
 for($i = 0; $i < count($matches); $i++){
     $match = $matches[$i];
-    echo $match["team"] . " " . "-" . " " . $match["guest"] . " | " . $match["teamScore"] . "-" . $match["guestScore"] . "<br>";
+    echo $match["home_team"] . " " . "vs" . " " . $match["guest_team"] . "  " . $match["home_score"] . "-" . $match["guest_score"] . "<br>";
 };
 
 ?>
